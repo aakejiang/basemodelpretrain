c90987cbbd4ee95292a4728674ec3d88b3a4650a
蒋科 <jiangke@zhejianglab.com>
2023-04-10 16:48:00 +0800

    fix

--- cifar-resnet.py
+++ cifar-resnet.py
@@ -32,7 +32,7 @@ model_names = sorted(name for name in models.__dict__
 
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10/100 Training')
 # Datasets
-parser.add_argument('-d', '--dataset', default='cifar100', type=str)
+parser.add_argument('-d', '--dataset', default='cifar10', type=str)
 parser.add_argument('-j', '--workers', default=0, type=int, metavar='N',
                     help='number of data loading workers (default: 0)')
 # Optimization options   508 
